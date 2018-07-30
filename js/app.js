b888f8ac2da7f82107b3e8ce8ef0c54b2a92ff7a
Kedar Bhusal <kedarbhusal@yahoo.com>
2018-07-30 23:30:53 +0545

    feat: Show score and players life
    Add a feature to show players score and life.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,15 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    // this.y = y;
+    // this.speed = speed;
+    this.y = Math.floor((Math.random() * 3) + 1)*70;
+    this.speed = Math.floor((Math.random() * 3) + 1)*150;
+
 };
 
 // Update the enemy's position, required method for game
@@ -17,6 +20,10 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed*dt;
+    //if x value is greater takes time to reach the canvas so changed it to nearest values.
+    if (this.x > 600) {
+        this.x = -200;
+    }
     this.onCollision();
 };
 
@@ -25,12 +32,12 @@ Enemy.prototype.onCollision = function() {
     let playerLoc = { x: player.x, y: player.y, width: 50, height: 50};
     let enemyLoc = { x: this.x, y: this.y, width: 50, height: 50};
 // check the collision and decrease life if true
-      if (playerLoc.x < enemy.x + enemyLoc.width &&
-        playerLoc.y < enemy.y + enemyLoc.height &&
+      if (playerLoc.x < enemyLoc.x + enemyLoc.width &&
+        playerLoc.y < enemyLoc.y + enemyLoc.height &&
         playerLoc.x + playerLoc.width > enemyLoc.x &&
         playerLoc.y + playerLoc.height > enemyLoc.y){
-        player.reset();
-        lives--;
+        player.lostLives();
+        player.playerReset();
     }
 }
 
@@ -51,7 +58,6 @@ let Player = function(x, y, speed) {
 }
 
 // @description update the position of player to keep withn the boundaries
-
 Player.prototype.update = function() {
     if(this.x < 0) {
         this.x = 0;
@@ -60,12 +66,29 @@ Player.prototype.update = function() {
         this.x = 400;
     }
     if(this.y < 0) {
-        this.y = 0;
+        player.addScore();
+        player.playerReset();
     }
     if(this.y > 400) {
         this.y = 400;
     }
+};
 
+Player.prototype.playerReset = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+Player.prototype.addScore = function() {
+    score +=25;
+    scoreCounts.innerHTML = `Score: ${score}`;
+};
+
+Player.prototype.lostLives = function() {
+    life--;
+    endGame();
+    let lives = document.getElementById('lives');
+    lives.removeChild(lives.firstElementChild);
 };
 
 Player.prototype.render = function() {
@@ -93,13 +116,34 @@ Player.prototype.handleInput = function(keyPressed) {
     }
 };
 
+function endGame() {
+    if(life === 0){
+    allEnemies = [];
+    gameInfo.innerHTML = `
+    <h1 class="heading-one">GAME OVER</h1>
+    <p></p>
+    <p class="text">Your score is ${score}.</p>
+    <button class="play-again" onclick="restart()">Play Again</button>
+    `;
+    messageBox.classList.remove('hidden');
+    }
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-let allEnemies = [];
+
+let allEnemies = [new Enemy(),new Enemy(),new Enemy(),new Enemy()];
+
 let player = new Player(200,400,50);
+let score = 0;
+let life = 3;
+let scoreCounts = document.querySelector('.score');
+let messageBox = document.querySelector('.complete-message');
+let gameInfo = document.querySelector('.popup');
+
 
 
 
